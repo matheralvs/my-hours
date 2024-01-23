6c73e9f797d79e721ce5d3eeddec2a260193424c
Matheus Alves <mtalves27@gmail.com>
2024-01-23 09:08:18 -0300

    chore: adjusted system tray and parts

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,50 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+use tauri::{
+    CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem,
+};
+
+fn on_system_tray_fn(app: &tauri::AppHandle, event: tauri::SystemTrayEvent) {
+    match event {
+        SystemTrayEvent::DoubleClick {
+            position: _,
+            size: _,
+            ..
+        } => {
+            let window = app.get_window("main").unwrap();
+            window.show().unwrap();
+        }
+        SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+            "quit" => {
+                std::process::exit(0);
+            }
+            "hide" => {
+                let window = app.get_window("main").unwrap();
+                window.hide().unwrap();
+            }
+            _ => {}
+        },
+        _ => {}
+    }
 }
 
 fn main() {
+    let _tray = SystemTray::new();
+
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let hide = CustomMenuItem::new("hide".to_string(), "Hide");
+
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(quit)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(hide);
+
+    let system_tray = _tray.with_menu(tray_menu);
+
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .system_tray(system_tray)
+        .on_system_tray_event(on_system_tray_fn)
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
